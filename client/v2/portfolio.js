9ae1ec27f96775fe385c4c610c72b3d61595423d
Art <arthur.bonneaud@edu.devinci.fr>
2022-01-31 15:25:00 +0100

    feature6 dont work

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -5,11 +5,15 @@
 let currentProducts = [];
 let currentPagination = {};
 
-// inititiqte selectors
+// inititiate selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
+const selectBrand = document.querySelector('#brand-select');
+const selectByReasonnablePrice = document.querySelector('#filter-reasonnable-price-select');
+const selectByRecentlyReleased = document.querySelector('#filter-recently-released-select');
+const selectSort = document.querySelector('#sort-select');
 
 /**
  * Set global value
@@ -96,11 +100,43 @@ const renderIndicators = pagination => {
   spanNbProducts.innerHTML = count;
 };
 
+
+/**
+ * Render brandselector
+ * @param {Array} products 
+ */
+
+ const renderBrands = products => {
+  const brandsNames = [];
+  products.forEach(product => {
+    if (!brandsNames.includes(product.brand)){
+      brandsNames.push(product.brand);
+    }
+  })
+  let options = ['<option value="select a brand">select a brand</option>']
+    options.push(Array.from(
+        { 'length': brandsNames.length },
+        (value, index) => `<option value="${brandsNames[index]}">${brandsNames[index]}</option>`
+    ).join(''));
+    options.push('<option value="show all brands">show all brands</option>')
+
+    selectBrand.innerHTML = options;
+};
+
+/**
+ * List of renders
+ */
+
 const render = (products, pagination) => {
   renderProducts(products);
   renderPagination(pagination);
   renderIndicators(pagination);
+  renderBrands(products); //feature 2
 };
+
+
+
+
 
 /**
  * Declaration of all Listeners
@@ -122,3 +158,84 @@ document.addEventListener('DOMContentLoaded', async () => {
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
+
+// Feature 1 - Browse pages 
+selectShow.addEventListener('change', event => {
+  fetchProducts(parseInt(event.target.value), currentPagination.pageSize)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination));
+});
+
+// Feature 2 - Filter by brands
+selectBrand.addEventListener('change', async (event) => {
+  var products = await fetchProducts(currentPagination.currentPage, currentPagination.pageSize);
+
+  if (event.target.value == "show all brands") {
+      products = await fetchProducts(currentPagination.currentPage, currentPagination.pageSize)   
+  }
+  else {
+      products.result = products.result.filter(item => item.brand == event.target.value)
+  }
+
+    setCurrentProducts(products);
+    render(currentProducts, currentPagination);
+});
+
+// Feature 3 - Filter by recent products
+
+selectByReasonnablePrice.addEventListener('change', async (event) => {
+  const products = await fetchProducts(currentPagination.currentPage, currentPagination.pageSize);
+
+  if (event.target.value == "By reasonable price"){
+    products.result = products.result.filter(product => product.price <= 50);
+  }
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+
+});
+
+// Feature 4 - Filter by reasonable price
+
+function recentlyReleased(product_date){
+  let now = new Date();
+  product_date = new Date(product_date);
+  return now - product_date;
+}
+
+
+selectByRecentlyReleased.addEventListener('change', async (event) => {
+  const products = await fetchProducts(currentPagination.currentPage, currentPagination.pageSize);
+
+  if (event.target.value == "By recently released"){
+    products.result = products.result.filter(product => recentlyReleased(product.released) <= (24 * 60 * 60 * 1000 * 14));
+  }
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+
+});
+
+// Feature 5 - Sort by price + Feature 6 - Sort by date
+
+selectSort.addEventListener('change', async (event) => {
+  switch (event.target.value) {
+    case 'price-asc':
+      currentProducts = currentProducts.sort((p1, p2) => { return p1.price - p2.price; });
+      break;
+    case 'price-desc':
+      currentProducts = currentProducts.sort((p1, p2) => { return p2.price - p1.price; });
+      break;
+    case 'date-asc':
+      currentProducts = currentProducts.sort((p1, p2) => { return new Date(p2.released) - new Date(p1.released); });
+      break;
+    case 'date-desc':
+      currentProducts = currentProducts.sort((p1, p2) => { return new Date(p1.released) - new Date(p2.released); });
+      break;
+    default:
+      break;
+  }
+  render(currentProducts, currentPagination);
+});
+
+
